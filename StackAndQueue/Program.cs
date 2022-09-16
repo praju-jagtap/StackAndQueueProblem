22a6349a76371ecbe0a10bba5ea720be977ef4e8
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-16 17:27:45 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack And Queue Problem statement");
+        }
+    }
+}
